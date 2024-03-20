8d58c448bda9ab60d008f6dcd39e4e8237f369a8
Daniel Lee <tur95049@temple.edu>
2024-03-20 16:06:38 -0400

    Add a fragment instance and use it instead of activity

---
+++ app/src/main/java/edu/temple/fragmentrefactor/NameFragment.kt
@@ -0,0 +1,32 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class NameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name, container, false).apply {
+            val nameDisplayTextView = findViewById<TextView>(R.id.textView)
+            val nameEditText = findViewById<EditText>(R.id.editTextText)
+            findViewById<Button>(R.id.button).setOnClickListener {
+                var name = nameEditText.text.toString()
+
+                nameDisplayTextView.text = if (name != "null") {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+            }
+        }
+    }
+}
